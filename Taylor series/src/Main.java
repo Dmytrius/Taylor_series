fe19f03c2497e88e6061e464c5fc8216ef9d58a6
Dmytro <boyko.dmitr@gmail.com>
2021-04-15 07:36:35 +0300

    rename Tailor function

--- Taylor series/src/Main.java
+++ Taylor series/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String arg[]) throws IOException {
 
         int fact = 1;
-        double sum = 1;
+        double sumA = 1;
         double term;
         int start = 1;
         int iter = 4;
@@ -13,8 +13,8 @@ public class Main {
         for (int i = 1; i < iter; i++) {
             fact *= i;
             term = Math.pow(start, i) / fact;
-            sum += term;
+            sumA += term;
         }
-        System.out.print("\n\n\tanswer = " + sum);
+        System.out.print("\n\n\tanswer = " + sumA);
     }
 }
